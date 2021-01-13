23a41db18c2ba288b857a12b86c28ecc6f29f682
Aurislucero <45070914+Aurislucero@users.noreply.github.com>
2021-01-12 20:14:55 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,15 +5,7 @@
 export const environment = {
   production: false,
   url:'http://localhost:5000/firestore-grafica-44390/us-central1',
-  firebase: {
-    apiKey: "AIzaSyDiAxOMe1my1s3Lj9vffqVSuB3cAKhpCPU",
-    authDomain: "firestore-grafica-44390.firebaseapp.com",
-    projectId: "firestore-grafica-44390",
-    storageBucket: "firestore-grafica-44390.appspot.com",
-    messagingSenderId: "263437014819",
-    appId: "1:263437014819:web:c7bf1b091b4dd58c70de96",
-    measurementId: "G-3NP9TR9R0K"
-  }
+
 };
 
 /*
